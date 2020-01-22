cc19fe2163edd12c7de115e1d4ffa8bef6cccb52
Doga Barsgan <dogabarsgan@Dogas-MacBook-Pro-2.local>
2020-01-21 22:26:25 -0500

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) =>{
+
+    const url = 'https://api.darksky.net/forecast/69a3fc2d3c568cb8ffb1851122e337d7/'+ latitude + ',' + longitude;
+    request({url, json:true }, (error, {body}) =>{
+        if(error){
+            callback('Unable to connect to weather services', undefined);
+        }else if(body.error){
+
+            callback('Unable to find location. Try another search.', undefined)
+        }else{
+            callback(undefined, 
+
+                body.daily.data[0].summary + ' It is currently ' + body.currently.temperature  + ' degrees out. There is a ' + body.currently.precipProbability +'% change of rain.')
+        }
+
+    });
+
+};
+
+module.exports = forecast;
